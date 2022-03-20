534b24ccea27f20aec4eca04633b58a478f69b8d
Nishat <lakhani.nishat1992@gmail.com>
2022-03-20 17:10:24 +0530

    Random String Generator with user input

--- src/com/games/RandomStringGenerator.java
+++ src/com/games/RandomStringGenerator.java
@@ -1,11 +1,17 @@
 package com.games;
 
+import java.util.Scanner;
+
 public class RandomStringGenerator {
 
 	public static void main(String[] args) {
 
 		String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
-		int len = 10;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the length of String to generate : ");
+		int len = sc.nextInt();
+		sc.close();
 		String generatedString = "";
 		
 		for (int i=0; i<len; i++)
